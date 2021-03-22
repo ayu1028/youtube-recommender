c5e8f50474c4acbea5440da37259baf4827cbffa
山盛歩実 <ayumiatmtlab@gmail.com>
2021-03-22 19:00:15 +0900

    change tabs to spaces

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ const config = {
 //youtube api information
 const searchUrl = 'https://www.googleapis.com/youtube/v3/search';
 const baseQuery = {
-	part: 'snippet',
-	type: 'video',
-	maxResults: 50,
-	key: process.env.YOUTUBE_SECRET
+  part: 'snippet',
+  type: 'video',
+  maxResults: 50,
+  key: process.env.YOUTUBE_SECRET
 };
 const baseVideoUrl = 'https://www.youtube.com/watch';
 
@@ -49,30 +49,30 @@ async function handleEvent(event) {
   }
 
 // youtube api searching
-	const searchText = event.message.text;
-	const query = { q: searchText };
-	const qs = querystring.stringify(Object.assign(baseQuery, query));
-	const requestUrl = util.format('%s?%s', searchUrl, qs);
-	console.log(requestUrl);
-	const ifnum = 1;
+  const searchText = event.message.text;
+  const query = { q: searchText };
+  const qs = querystring.stringify(Object.assign(baseQuery, query));
+  const requestUrl = util.format('%s?%s', searchUrl, qs);
+  console.log(requestUrl);
+  const ifnum = 1;
 
-	const searchResults = await axios.get(requestUrl);
-	const videoQuery = { v: searchResults.data.items[0].id.videoId };
-	const vqs = querystring.stringify(videoQuery);
-	const videoUrl = util.format('%s?%s', baseVideoUrl, vqs);
-	console.log(videoUrl);
+  const searchResults = await axios.get(requestUrl);
+  const videoQuery = { v: searchResults.data.items[0].id.videoId };
+  const vqs = querystring.stringify(videoQuery);
+  const videoUrl = util.format('%s?%s', baseVideoUrl, vqs);
+  console.log(videoUrl);
 
-	  // create a echoing text message
-		const echo =[
-			{
-				type: 'text',
-				text: `"${searchText}"のレコメンド動画はこちらです！`
-			},
-			{
-				type: 'text',
-				text: videoUrl
-			}
-		];
+    // create a echoing text message
+    const echo =[
+      {
+        type: 'text',
+        text: `"${searchText}"のレコメンド動画はこちらです！`
+      },
+      {
+        type: 'text',
+        text: videoUrl
+      }
+    ];
 
   // use reply API
   return client.replyMessage(event.replyToken, echo);
